80d537f1d66679a2e76d5d25c76a1d80cb489fc2
RISHIKESHAVAN <44740714+RISHIKESHAVAN@users.noreply.github.com>
2022-04-09 20:08:21 +0200

    comments added

--- src/my-asset-contract.ts
+++ src/my-asset-contract.ts
@@ -8,9 +8,19 @@ import { MyAsset } from './my-asset';
 @Info({title: 'MyAssetContract', description: 'My Smart Contract' })
 export class MyAssetContract extends Contract {
 
+    // @Transaction() decorator indicates that this method can be called 
+    // to generate a transaction response for this smart contract.
+    // @Transaction(false) indicates that it only reads from the ledger
+    // @Transaction(true) means that it can also write to the ledger
+    // Context object is used by the smart contract to maintain user data across contract
+    // This method returns true or false depending on whether `myAssetId`
+    // was in the ledger.
     @Transaction(false)
     @Returns('boolean')
     public async myAssetExists(ctx: Context, myAssetId: string): Promise<boolean> {
+        // Each method uses the world state to read the current value of a set of 
+        // business objects and generate the corresponding new values for those 
+        // objects. The Context object (ctx) gives you direct access to the world state
         const data: Uint8Array = await ctx.stub.getState(myAssetId);
         return (!!data && data.length > 0);
     }
@@ -24,6 +34,8 @@ export class MyAssetContract extends Contract {
         const myAsset: MyAsset = new MyAsset();
         myAsset.value = value;
         const buffer: Buffer = Buffer.from(JSON.stringify(myAsset));
+        // The putState method changes the current value of a business object 
+        // with the key myAssetId to the value buffer. 
         await ctx.stub.putState(myAssetId, buffer);
     }
 
